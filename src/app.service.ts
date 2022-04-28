5a760c258ff89b3d1b9e750ba91d5935d03f0ba2
Quan Kori <k0r1@Quans-MacBook-Pro.local>
2022-04-28 15:59:33 +0700

    Connect network

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
-
+import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
+import { SigningStargateClient } from '@cosmjs/stargate';
+import config from './configs/config';
+import { stringToPath } from '@cosmjs/crypto';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<any> {
+    const mnemonic = config.network.mnemonic;
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
+      prefix: 'archway',
+    });
+    const [firstAccount] = await wallet.getAccounts();
+    console.log(firstAccount.address);
+
+    const rpcEndpoint = config.network.rpc;
+    const client = await SigningStargateClient.connectWithSigner(
+      rpcEndpoint,
+      wallet,
+    );
+    const tx = await client.getAllBalances(firstAccount.address);
+    console.log(tx);
+    return firstAccount.address;
   }
 }
