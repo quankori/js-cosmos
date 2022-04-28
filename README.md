77486f8ed3dc22541b5b6e3ff180692801c8906a
Quan Kori <k0r1@Quans-MacBook-Pro.local>
2022-04-28 15:23:15 +0700

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project Cosmos Bot
+
---
+++ src/configs/config.ts
@@ -0,0 +1,16 @@
+// Library
+import * as dotenv from 'dotenv';
+
+// Dotenv
+dotenv.config();
+export default {
+  envServer: {
+    host: process.env.APP_HOST || 'localhost',
+    port: Number(process.env.APP_PORT).valueOf() || 3000,
+    type: process.env.NODE_ENV || 'local',
+  },
+  network: {
+    rpc: process.env.NETWORK_RPC || '',
+    mnemonic: process.env.KEY_MNENONICE || '',
+  },
+};
